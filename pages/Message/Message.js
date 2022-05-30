0c52ba7e186e4d62045b21d6a74df7fc5f4a9850
Bramcoen <coenders123@gmail.com>
2022-05-30 09:52:49 +0200

    Update Message.js

--- pages/Message/Message.js
+++ pages/Message/Message.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import { useSession} from "next-auth/react"
 
 export default function Message({message}) {
-    const { data: session } = useSession()
-    console.log(session);
-    return (<h1>{message.text}</h1>
+    return (<h1>{message}</h1>
     )
 }
 
