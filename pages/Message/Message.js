6e9743b0245ea171c160a0b2de3045e323617050
Bramcoen <coenders123@gmail.com>
2022-06-02 15:23:26 +0200

    Removed unused components and code cleanup

--- pages/Message/Message.js
+++ pages/Message/Message.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export default function Message({message}) {
+const Message = ({message}) => {
     return (<>
-
     <div className="border border-secondary">
-    <h1>{message.text}</h1>
             <text>{message.creationDate}</text>
 </div>
         </>
     )
 }
+
+
+export default Message;
